7e4f50b08b9dbdaed63ddd4806f6f26ac67c72f9
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-02-18 16:04:33 -0300

    Enviando requisito 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
       <main>
         <Header />
+        <SolarSystem />
         <p>Sistema Solar</p>
       </main>
     );

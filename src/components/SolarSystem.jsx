cfb997c000f3d845aa173dbc561bd133988a0c2f
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-02-18 17:09:20 -0300

    Enviando requisito 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
+import planets from '../data/planets';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <ul>
+          {planets.map((planet) => (
+            <li key={ planet.name }>
+              <PlanetCard
+                planetName={ planet.name }
+                planetImage={ planet.image }
+              />
+            </li>))}
+        </ul>
       </div>
     );
   }
